72d1919240981a8d8bfc32346a17e1abdba3c728
bonom <andrea.bonomi489@gmail.com>
2022-08-24 17:33:02 +0200

    pickle support colab

--- classes/Car.py
+++ classes/Car.py
@@ -2,7 +2,10 @@ from random import SystemRandom
 import pandas as pd
 import numpy as np
 import os
-import pickle
+try:
+    from pickle5 import pickle
+except ImportError:
+    import pickle
 import plotly.express as px
 import math
 
